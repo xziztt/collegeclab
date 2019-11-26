1e7062151da4b082c27613a5e22f355027a1ef4e
xziztt <vigneshmvm2000@gmail.com>
2019-11-26 01:03:48 -0500

    lol

---
+++ programs/doublestack.c
@@ -0,0 +1,80 @@
+#include<stdio.h>
+#include<stdlib.h>
+int queue[10];int top1=-1,top2=9;
+void push1(){
+    int data;
+    scanf("%d",&data);
+    if(top1==-1){
+        top1=0;
+    }
+    else if(top1+1==top2){
+        printf("\n overflow");
+        return;
+    }
+    else 
+        top1++;
+    queue[top1]=data;
+
+}
+void push2(){
+    int data;
+    scanf("%d",&data);
+    if(top2==10){
+        top2--;
+    }
+    else if(top1+1==top2){
+        printf("\n overflow");
+        return;
+        }
+    else
+        top2--;
+    queue[top2]=data;
+}
+void pop1(){
+    printf("\n pop1");
+    if(top1==-1){
+        printf("\n underflow");
+
+    }
+    else 
+        top1--;
+}
+void pop2(){
+    printf("\n pop2");
+    if(top2==10){
+        printf("\n underflow");
+
+
+    }
+    else
+    {
+            top2++;
+    }
+    
+}
+
+void printx(){
+    int i;int temp1,temp2;
+    temp1=top1;
+    temp2=top2;
+    for(i=temp1;i>=0;i--){
+        printf(" %d ",queue[i]);
+    }
+    for(i=temp2;i<9;i++){
+        printf(" %d ",queue[i]);
+    }
+}
+void main(){
+    int i;
+    for(i=0;i<2;i++){
+        push1();
+    }
+    for(i=0;i<2;i++){
+        push2();
+    }
+
+    printx();
+    pop1();pop2();
+    printx();
+    
+}
